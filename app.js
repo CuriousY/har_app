eef55be41b49fddf443f3404c76457ba33f2a783
Nitesh <nickscorpio4u@gmail.com>
2018-03-21 17:13:20 +0530

    configurable properties for header and footer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
       path = require('path')
       request = require('request')
-      app = express();
+      app = express(),
+      commonJSON = require('./config/common.json');
 
 const port = process.env.PORT || 8080;
 
@@ -10,27 +11,29 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 
 app.get('/',(request,response) => {
-    response.render('home')
+    response.render('home',{commonObj:commonJSON})
 })
 
 app.get('/about',(request,response) => {
-    response.render('about')
+    response.render('about',{commonObj:commonJSON})
 })
 
 app.get('/services',(request,response) => {
-    response.render('services')
+    response.render('services',{commonObj:commonJSON})
 })
 
 app.get('/contact',(request,response) => {
-    response.render('contact')
+    response.render('contact',{commonObj:commonJSON})
 })
 
 app.get('/gallery',(request,response) => {
-    response.render('gallery')
+    response.render('gallery',{commonObj:commonJSON})
 })
 
-
+app.get('*',(request,response) => {
+    response.render('home',{commonObj:commonJSON})
+})
 
 app.listen(port,() => {
-    console.log('listening to 3000')
+    console.log(`listening on ${port}`);
 })
